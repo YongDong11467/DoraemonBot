161ec7c6a9c75cc5802e43b93ab6d51c07b43eea
ClemsonJames <qingl114@hotmail.com>
2020-03-11 12:05:58 -0400

    error handler and misc

---
+++ cogs/error.py
@@ -0,0 +1,30 @@
+import sys
+import traceback
+import discord
+from discord.ext import commands
+
+
+class ErrorHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send('This command cannot be used in private messages.')
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.author.send('Sorry. This command is disabled and cannot be used.')
+        elif isinstance(error, commands.CommandInvokeError):
+            original = error.original
+            if not isinstance(original, discord.HTTPException):
+                print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
+                traceback.print_tb(original.__traceback__)
+                print(f'{original.__class__.__name__}: {original}', file=sys.stderr)
+        elif isinstance(error, commands.ArgumentParsingError):
+            await ctx.send(error)
+        elif isinstance(error, commands.CommandNotFound):
+            await ctx.send('This command does not exist, use !help to see all the commands')
+
+
+def setup(bot):
+    bot.add_cog(ErrorHandler(bot))
---
+++ cogs/music.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx):
+        if not ctx.author.voice:
+            await ctx.send('You are not connected to a voice channel.')
+        channel = ctx.message.author.voice.channel
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+        await ctx.send(f'Joined {channel}')
+
+
+def setup(bot):
+    bot.add_cog(Music(bot))
